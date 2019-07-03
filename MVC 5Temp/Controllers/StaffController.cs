d3d638ac0c83d982a6ccbe7dd5806b4a2e00a6ce
nar300 <ni781233@gmail.com>
2019-07-03 12:35:49 +0100

    bin

---
+++ MVC 5Temp/Controllers/StaffController.cs
@@ -0,0 +1,61 @@
+﻿using MVC_5Temp.Data;
+using MVC_5Temp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_5Temp.Controllers
+{
+    public class StaffController : Controller
+    {
+        CustomerDBContext db = new CustomerDBContext();
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+        // GET: Staff
+        public ActionResult Index()
+        {
+            var customer = db.Customers.ToList();
+            return View(customer);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var customer = db.Customers.Find(id);
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult Edit(Customer customer)
+        {
+            db.Entry(customer).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+    }
+}
